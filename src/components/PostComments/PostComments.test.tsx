c30cc0c62fe9b84871bc64bafce435c62fa83253
wigor <wigor21@hotmail.com>
2023-10-08 15:19:08 -0300

    Teste para adicionar dois comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,31 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+  test("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  test("Deve adicionar dois comentários", () => {
+    const { debug } = render(<PostComment />);
+    fireEvent.change(screen.getByTestId("field-comments"), {
+      target: {
+        value: "Que perfeição!",
+      },
     });
+    fireEvent.click(screen.getByTestId("btn-submit"));
+
+    fireEvent.change(screen.getByTestId("field-comments"), {
+      target: {
+        value: "Miniatura top do Batmóvel!",
+      },
+    });
+    fireEvent.click(screen.getByTestId("btn-submit"));
+
+    debug();
+    expect(screen.getByText("Que perfeição!")).toBeInTheDocument();
+    expect(screen.getByText("Miniatura top do Batmóvel!")).toBeInTheDocument();
+  });
 });
